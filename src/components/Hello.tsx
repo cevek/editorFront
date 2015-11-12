6ed54d1e5bd31fa0d1b35cad1eef4125eb18dd33
cody <cevek@ya.ru>
2015-11-12 22:22:08 +0500

    Added some classes

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,4 +1,31 @@
 require('./main.css');
 import * as React from 'react';
 
+class Line {
+    constructor(public en:TextLine, public ru:TextLine) {}
+}
+
+class TextLine {
+    words:string[] = [];
+    private text:string;
+
+    constructor(public start:number, public dur:number, text:string) {
+        this.setText(text);
+    }
+
+    setText(text:string) {
+        this.text = text;
+        this.words = text.split(/\s+/);
+    }
+
+    getText() {
+        return this.text;
+    }
+
+}
+
+class Hello extends React.Component<{},{}> {
+    lines:Line[] = [];
+}
+
 React.render(<div>Hello</div>, document.body);
